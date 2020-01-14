096763012234413e132ee230849c238fa1f3a51b
DoHyeon <whdies816@gmail.com>
2020-01-14 22:50:36 +0900

    feat(Docker/superdocker/main_server) - server supervising 용 페이지 개설 - API action중 서버 연결 테스트도 필요할 것으로 보임

---
+++ Server/Docker/superdocker/main_server/views/check/check.js
@@ -0,0 +1,27 @@
+const key_list = ['db', '11st', 'joongna'];
+
+const button_list = key_list.map(key =>
+    document.getElementById(`${key}_button`).addEventListener('click', e => {
+        const message = key_to_fn[key];
+        console.log(message);
+        document.getElementById(`${key}_code`).textContent = message;
+    }),
+);
+
+const checkDBServer = () => {
+    return 'DB';
+};
+
+const check11stServer = () => {
+    return '11st';
+};
+
+const checkJoongnaServer = () => {
+    return 'joongna';
+};
+
+const key_to_fn = {
+    'db': checkDBServer(),
+    '11st': check11stServer(),
+    'joongna': checkJoongnaServer(),
+};
