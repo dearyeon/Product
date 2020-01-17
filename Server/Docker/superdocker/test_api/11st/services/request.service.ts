bf133c246dc592cb59b9a1e1d0cc7fd4046e9802
DoHyeon <whdies816@gmail.com>
2020-01-18 00:50:29 +0900

    add(/Server/Main_Server): 서버 파일 위치 변경

--- Server/Docker/superdocker/test_api/11st/services/request.service.ts
+++
@@ -1,7 +0,0 @@
-import * as rp from 'request-promise';
-import { APISite } from '../product_search/src/option';
-import { SiteRequest } from '../product_search/interface/SiteRequest.interface';
-
-export const requestService = async (qs: SiteRequest) => {
-    return rp.get({ url: APISite, qs, json: true, encoding: null });
-};
--- Server/Docker/superdocker/test_api/joongna/product_search/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { scrapComponent } from './scrap.component';
-
-export const index = async (keyword: string) => {
-    return scrapComponent(keyword)
-        .then(value => value)
-        .catch(err => {
-            throw new Error(err);
-        });
-};
