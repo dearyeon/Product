61422062c198cfded11bdf24fd78584a895d85b2
DoHyeon <whdies816@gmail.com>
2020-01-18 00:31:43 +0900

    Elevenst_product_list 완료

--- API/a000/product_list/index.ts
+++ API/a000/product_list/index.ts
@@ -1,9 +1,5 @@
 import { scrapComponent } from './src/scrap.component';
 
 export const index = async (keyword: string) => {
-    return scrapComponent(keyword)
-        .then(value => value)
-        .catch(err => {
-            throw new Error(err);
-        });
+    return scrapComponent(keyword).then(value => value);
 };
--- API/a000/product_list/src/request.service.ts
+++ API/a000/product_list/src/request.service.ts
@@ -1,5 +1,5 @@
 import * as rp from 'request-promise';
-import { APISite } from '../option';
+import { APISite } from '../../option';
 
 export const requestService = async (qs: any) => {
     return rp.get({ url: APISite, qs, json: true, encoding: null });
