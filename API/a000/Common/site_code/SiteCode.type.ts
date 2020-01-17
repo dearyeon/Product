7d985c893ce1362f6f729c6af61dacdb29dbda1a
DoHyeon <whdies816@gmail.com>
2020-01-18 00:20:33 +0900

    interface update 반영

--- API/a000/Common/site_code/SiteCode.type.ts
+++ API/a000/Common/site_code/SiteCode.type.ts
@@ -1,12 +1,15 @@
 /** 사이트 코드
+ * + Elevenst: 000
  * + BeonGae: 001
  * + JoongGo: 002
  * + Daangn: 003
  * + SellIt: 004
  * + Hello: 005
  */
-export type SiteCode = BeonGae | JoongGo | Daangn | SellIt | Hello;
+export type SiteCode = Elevenst | BeonGae | JoongGo | Daangn | SellIt | Hello;
 
+/** Elevenst 11번가(test 전용) */
+type Elevenst = '000';
 /** BeonGae : 번개장터 */
 type BeonGae = '001';
 /** JoonGo : 중고나라 */
