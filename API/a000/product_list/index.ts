312a4912f0abc81918ffd6448abbec47aef685b1
DoHyeon <whdies816@gmail.com>
2020-01-18 01:33:35 +0900

    input 문제로 인한 수정

--- API/a000/product_list/index.ts
+++ API/a000/product_list/index.ts
@@ -1,5 +1,6 @@
 import { scrapComponent } from './src/scrap.component';
+import { ProductListRequest } from '../Common/action';
 
-export const index = async (keyword: string) => {
-    return scrapComponent(keyword).then(value => value);
+export const index = async (keyword: ProductListRequest) => {
+    return scrapComponent(keyword.search_word).then(value => value);
 };
--- API/a000/product_list/product_list.e2e.ts
+++ API/a000/product_list/product_list.e2e.ts
@@ -1,6 +1,6 @@
 import { index } from '.';
 
-index('모니터')
+index({ search_word: '모니터' })
     .then(value => console.log(value))
     .catch(err => {
         throw new Error(err);
