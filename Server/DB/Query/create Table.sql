d6d2c310f9b6d504bb82f0986045005e587a2205
beoms <bm1478@naver.com>
2020-01-20 20:09:39 +0900

    change account_info

--- Server/DB/Query/create Table.sql
+++ Server/DB/Query/create Table.sql
@@ -1,13 +1,16 @@
 show databases;
 use kj_product;
 show tables;
+drop table product_list;
 
 create table product_list (
-	id integer not null auto_increment primary key,
-    site_code integer not null,
+	id integer not null auto_increment,
+    site_code varchar(10) not null,
     title varchar(100) not null,
     price integer not null,
-    thumnail varchar(200)
+    thumnail varchar(200),
+    primary key(id)
 );
 
 select * from product_list;
+alter table product_list convert to charset utf8;
