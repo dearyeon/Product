3033f5303f0194a3fceaf54e798819531b7e0dcb
wjp103 <wjp103@naver.com>
2020-01-29 20:51:35 +0900

    add many things

--- Layout/src/components/ProductContents.js
+++ Layout/src/components/ProductContents.js
@@ -1,48 +1,19 @@
 import React, { Component } from "react"
 import ProductImage from "./utils/ProductImage"
-import SiteIcon from "./utils/SiteIcon"
 import HashTag from "./HashTag"
+import SiteTitle from "./utils/SiteTitle"
+
 import "../style/ProductContents.scss"
-import "../style/ImageComponent.scss"
 
 class SiteTileComponent extends Component {
-    constructor(props) {
-        super(props)
-        const { site_code } = this.props.data
-        this.state = {
-            site_title: this.selectSiteIcon(site_code)
-        }
-    }
-    selectSiteIcon = (site_code) => {
-        switch (site_code) {
-            case "000":
-                return "11번가"
-            case "001":
-                return "번개장터"
-            case "002":
-                return "중고나라"
-            case "003":
-                return "당근마켓"
-            case "004":
-                return "셀잇"
-            case "005":
-                return "헬로마켓"
-            default:
-                return ""
-        }
-    }
     render() {
         const { site_code, title, price, origin_url, detail, location, shipPrice } = this.props.data
-        const site_title = this.state.site_title
         return (
             <>
                 <div className="productMain">
                     <ProductImage src="" className="productDetailImage" />
                     <div className="productMainDetail">
-                        <div className="siteIconTitle">
-                            <SiteIcon site_code={site_code} />
-                            <div className="siteTitle">{site_title}</div>
-                        </div>
+                        <SiteTitle site_code={site_code}></SiteTitle>
                         <div className="title">{title}</div>
                         <div className="price">{price} 원</div>
                         <a href={origin_url} className="originUrl">바로가기</a>
